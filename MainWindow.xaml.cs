72de8477ea606899e69988be61afa11fed1727f3
RadoslawGarlicki <100523039+RadoslawGarlicki@users.noreply.github.com>
2022-06-26 11:43:58 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt1WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int clickedtimes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            clickedtimes = 0;
+        }
+
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            clickedtimes++;
+            MainLabel.Content = "klikłeś przycisk " + clickedtimes + " razy.";
+        }
+    }
+}
